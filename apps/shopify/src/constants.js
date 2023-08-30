d45d5987dbdd90c3a8842b99eaad76dc727923cf
anho <863612+anho@users.noreply.github.com>
2023-08-30 14:54:23 +0200

    chore: rename SKU_ID to ENTITY_TYPE

--- apps/shopify/src/constants.js
+++ apps/shopify/src/constants.js
@@ -5,24 +5,24 @@ export const DEFAULT_SHOPIFY_VARIANT_TITLE = 'Default Title';
 export const SHOPIFY_API_VERSION = '2023-01';
 export const SHOPIFY_ENTITY_LIMIT = 250;
 
-export const SKU_ID = {
+export const ENTITY_TYPE = {
   product: 'product',
   variant: 'variant',
   collection: 'collection',
-}
+};
 
 export const SKU_TYPES = [
   {
-    id: SKU_ID.product,
+    id: ENTITY_TYPE.product,
     name: 'Product',
   },
   {
-    id: SKU_ID.variant,
+    id: ENTITY_TYPE.variant,
     name: 'Product variant',
     default: true,
   },
   {
-    id: SKU_ID.collection,
+    id: ENTITY_TYPE.collection,
     name: 'Collection',
   },
 ];
--- apps/shopify/src/dataTransformer.js
+++ apps/shopify/src/dataTransformer.js
@@ -1,7 +1,7 @@
 import get from 'lodash/get';
 import flatten from 'lodash/flatten';
-import {DEFAULT_SHOPIFY_VARIANT_TITLE, SKU_ID} from './constants';
-import {convertBase64ToString, convertStringToBase64} from './utils/base64';
+import { DEFAULT_SHOPIFY_VARIANT_TITLE, ENTITY_TYPE } from './constants';
+import { convertBase64ToString, convertStringToBase64 } from './utils/base64';
 
 /**
  * Decodes the ID of a Shopify resource
@@ -9,8 +9,8 @@ import {convertBase64ToString, convertStringToBase64} from './utils/base64';
  * e.g. gid://shopify/Product/1234567890 -> 1234567890
  */
 export const decodeId = (sku) => {
-    const decodedId = convertBase64ToString(sku);
-    return decodedId && decodedId.slice(decodedId.lastIndexOf('/') + 1);
+  const decodedId = convertBase64ToString(sku);
+  return decodedId && decodedId.slice(decodedId.lastIndexOf('/') + 1);
 };
 
 /**
@@ -18,10 +18,10 @@ export const decodeId = (sku) => {
  * This is a QOL for users who copy-paste the URL from the browser
  */
 export const removeHttpsAndTrailingSlash = (url) => {
-    const protocol = /^https?:\/\/(www\.)?/;
-    const trailingSlash = /\/$/;
+  const protocol = /^https?:\/\/(www\.)?/;
+  const trailingSlash = /\/$/;
 
-    return url.replace(protocol, '').replace(trailingSlash, '');
+  return url.replace(protocol, '').replace(trailingSlash, '');
 };
 
 /**
@@ -29,28 +29,28 @@ export const removeHttpsAndTrailingSlash = (url) => {
  * the product schema expected by the SkuPicker component
  */
 export const collectionDataTransformer = (collection, apiEndpoint) => {
-    const image = get(collection, ['image', 'src'], '');
-    const handle = get(collection, ['handle'], undefined);
+  const image = get(collection, ['image', 'src'], '');
+  const handle = get(collection, ['handle'], undefined);
 
-    const collectionId = decodeId(collection.id);
-    const externalLink = apiEndpoint && collectionId && `https://${removeHttpsAndTrailingSlash(apiEndpoint)}/admin/collections/${collectionId}`;
-    const productsCount =  collection.products?.edges?.length ?? 0
+  const collectionId = decodeId(collection.id);
+  const externalLink = apiEndpoint && collectionId && `https://${removeHttpsAndTrailingSlash(apiEndpoint)}/admin/collections/${collectionId}`;
+  const productsCount = collection.products?.edges?.length ?? 0;
 
-    return {
-        id: collection.id,
-        image,
-        name: collection.title,
-        description: collection.description,
-        displaySKU: handle ? `Handle: ${handle}` : `Collection ID: ${collection.id}`,
-        sku: collection.id,
-        ...{externalLink},
-        additionalData: {
-            type: SKU_ID.collection,
-            description: collection.description,
-            updatedAt: collection.updatedAt,
-            productsCount: productsCount > 100 ? `> 100` : productsCount
-        },
-    };
+  return {
+    id: collection.id,
+    image,
+    name: collection.title,
+    description: collection.description,
+    displaySKU: handle ? `Handle: ${handle}` : `Collection ID: ${collection.id}`,
+    sku: collection.id,
+    ...{ externalLink },
+    additionalData: {
+      type: ENTITY_TYPE.collection,
+      description: collection.description,
+      updatedAt: collection.updatedAt,
+      productsCount: productsCount > 100 ? `> 100` : productsCount,
+    },
+  };
 };
 
 /**
@@ -58,48 +58,46 @@ export const collectionDataTransformer = (collection, apiEndpoint) => {
  * the product schema expected by the SkuPicker component
  */
 export const productDataTransformer = (product, apiEndpoint) => {
-    const image = get(product, ['images', 0, 'src'], '');
-    const sku = get(product, ['variants', 0, 'sku'], undefined);
+  const image = get(product, ['images', 0, 'src'], '');
+  const sku = get(product, ['variants', 0, 'sku'], undefined);
 
-    const productId = decodeId(product.id);
-    const externalLink = apiEndpoint && productId && `https://${removeHttpsAndTrailingSlash(apiEndpoint)}/admin/products/${productId}`;
+  const productId = decodeId(product.id);
+  const externalLink = apiEndpoint && productId && `https://${removeHttpsAndTrailingSlash(apiEndpoint)}/admin/products/${productId}`;
 
-    let price = 'no variants found';
+  let price = 'no variants found';
 
-    if (!!product.variants?.length) {
-        const firstPriceCurrencyCode = product.variants[0].price.currencyCode;
+  if (!!product.variants?.length) {
+    const firstPriceCurrencyCode = product.variants[0].price.currencyCode;
 
-        const prices = product.variants
-            .filter((variant) => variant.price.currencyCode === firstPriceCurrencyCode)
-            .map((variant) => variant.price.amount)
-            .sort();
+    const prices = product.variants
+      .filter((variant) => variant.price.currencyCode === firstPriceCurrencyCode)
+      .map((variant) => variant.price.amount)
+      .sort();
 
-        const formatter = currencyFormatter(firstPriceCurrencyCode);
-        const firstPrice = prices[0]
-        const lastPrice = prices[prices.length - 1]
+    const formatter = currencyFormatter(firstPriceCurrencyCode);
+    const firstPrice = prices[0];
+    const lastPrice = prices[prices.length - 1];
 
-        price = firstPrice === lastPrice
-            ? formatter.format(firstPrice)
-            : `${formatter.format(firstPrice)} - ${formatter.format(lastPrice)}`;
-    }
+    price = firstPrice === lastPrice ? formatter.format(firstPrice) : `${formatter.format(firstPrice)} - ${formatter.format(lastPrice)}`;
+  }
 
-    return {
-        id: product.id,
-        image,
-        name: product.title,
-        displaySKU: sku ? `SKU: ${sku}` : `Product ID: ${product.id}`,
-        sku: product.id,
-        description: product.description,
-        ...{externalLink},
-        additionalData: {
-            type: SKU_ID.product,
-            createdAt: product.createdAt,
-            updatedAt: product.updatedAt,
-            vendor: product.vendor,
-            price,
-            quantityAvailable: 'Depending on variants',
-        },
-    };
+  return {
+    id: product.id,
+    image,
+    name: product.title,
+    displaySKU: sku ? `SKU: ${sku}` : `Product ID: ${product.id}`,
+    sku: product.id,
+    description: product.description,
+    ...{ externalLink },
+    additionalData: {
+      type: ENTITY_TYPE.product,
+      createdAt: product.createdAt,
+      updatedAt: product.updatedAt,
+      vendor: product.vendor,
+      price,
+      quantityAvailable: 'Depending on variants',
+    },
+  };
 };
 
 /**
@@ -107,71 +105,70 @@ export const productDataTransformer = (product, apiEndpoint) => {
  * the product schema expected by the SkuPicker component
  */
 export const productVariantDataTransformer = (product) => {
-    const image = get(product, ['image', 'src'], '');
-    const sku = get(product, ['sku'], '');
-    const variantSKU = get(product, ['variantSKU'], '');
+  const image = get(product, ['image', 'src'], '');
+  const sku = get(product, ['sku'], '');
+  const variantSKU = get(product, ['variantSKU'], '');
 
-    return {
-        id: product.id,
-        image,
-        name: product.title,
-        displaySKU: variantSKU ? `SKU: ${variantSKU}` : `Product ID: ${sku}`,
-        sku,
-        additionalData: {
-            type: SKU_ID.variant,
-            ...product,
-        },
-    };
+  return {
+    id: product.id,
+    image,
+    name: product.title,
+    displaySKU: variantSKU ? `SKU: ${variantSKU}` : `Product ID: ${sku}`,
+    sku,
+    additionalData: {
+      type: ENTITY_TYPE.variant,
+      ...product,
+    },
+  };
 };
 
 export const productsToVariantsTransformer = (products) =>
-    flatten(
-        products.map((product) => {
-            const variants = product.variants.map((variant) => ({
-                ...variant,
-                variantSKU: variant.sku,
-                id: convertStringToBase64(variant.id),
-                sku: convertStringToBase64(variant.id),
-                productId: product.id,
-                description: product.description,
-                title: variant.title === DEFAULT_SHOPIFY_VARIANT_TITLE ? product.title : `${product.title} (${variant.title})`,
-                additionalData: {
-                    type: SKU_ID.variant,
-                    ...product,
-                },
-            }));
-            return variants;
-        })
-    );
+  flatten(
+    products.map((product) => {
+      const variants = product.variants.map((variant) => ({
+        ...variant,
+        variantSKU: variant.sku,
+        id: convertStringToBase64(variant.id),
+        sku: convertStringToBase64(variant.id),
+        productId: product.id,
+        description: product.description,
+        title: variant.title === DEFAULT_SHOPIFY_VARIANT_TITLE ? product.title : `${product.title} (${variant.title})`,
+        additionalData: {
+          type: ENTITY_TYPE.variant,
+          ...product,
+        },
+      }));
+      return variants;
+    })
+  );
 
 export const previewsToProductVariants =
-    ({apiEndpoint}) =>
-        ({sku, id, image, product, title, quantityAvailable, price}) => {
-            const productId = decodeId(product.id);
-            const externalLink = apiEndpoint && productId && `https://${removeHttpsAndTrailingSlash(apiEndpoint)}/admin/products/${productId}`;
+  ({ apiEndpoint }) =>
+  ({ sku, id, image, product, title, quantityAvailable, price }) => {
+    const productId = decodeId(product.id);
+    const externalLink = apiEndpoint && productId && `https://${removeHttpsAndTrailingSlash(apiEndpoint)}/admin/products/${productId}`;
 
-            return {
-                id,
-                image: get(image, ['src'], ''),
-                // TODO: Remove sku:id when @contentful/ecommerce-app-base supports internal IDs
-                // as an alternative piece of info to persist instead of the SKU.
-                // For now this is a temporary hack.
-                sku: id,
-                displaySKU: sku ? `SKU: ${sku}` : `Product ID: ${id}`,
-                productId: product.id,
-                name: title === DEFAULT_SHOPIFY_VARIANT_TITLE ? product.title : `${product.title} (${title})`,
-                ...{externalLink},
-                description: product.description,
-                additionalData: {
-                    type: SKU_ID.product,
-                    quantityAvailable,
-                    createdAt: product.createdAt,
-                    updatedAt: product.updatedAt,
-                    vendor: product.vendor,
-                    price: !!price.amount ? currencyFormatter(price.currencyCode).format(parseFloat(price.amount)) : 'unknown',
-                },
-            };
-        };
+    return {
+      id,
+      image: get(image, ['src'], ''),
+      // TODO: Remove sku:id when @contentful/ecommerce-app-base supports internal IDs
+      // as an alternative piece of info to persist instead of the SKU.
+      // For now this is a temporary hack.
+      sku: id,
+      displaySKU: sku ? `SKU: ${sku}` : `Product ID: ${id}`,
+      productId: product.id,
+      name: title === DEFAULT_SHOPIFY_VARIANT_TITLE ? product.title : `${product.title} (${title})`,
+      ...{ externalLink },
+      description: product.description,
+      additionalData: {
+        type: ENTITY_TYPE.product,
+        quantityAvailable,
+        createdAt: product.createdAt,
+        updatedAt: product.updatedAt,
+        vendor: product.vendor,
+        price: !!price.amount ? currencyFormatter(price.currencyCode).format(parseFloat(price.amount)) : 'unknown',
+      },
+    };
+  };
 
-const currencyFormatter = (currency) =>
-    new Intl.NumberFormat('en-US', {style: 'currency', currency});
+const currencyFormatter = (currency) => new Intl.NumberFormat('en-US', { style: 'currency', currency });
