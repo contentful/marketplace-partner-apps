7b4a707b019f67dce7feccf042c46089a38a97c8
weronika-kurczyna <117282008+weronika-kurczyna@users.noreply.github.com>
2023-09-14 02:29:44 +0200

    Voucherify - Contentful (#251)
    * Upload voucherify app
    
    * update ConfigScreen to match the other integrations style
    
    * validateCustomURL as useCallback
    
    * prettier formatting
    
    * changelog, license, readme
    
    * Update apps/voucherify/package.json
    
    Co-authored-by: Lisa White <whitelisab@gmail.com>
    
    * update package.json and default value of useState
    
    * update LICENSE
    
    * update LICENSE
    
    * Update correct production app definition ID
    
    ---------
    
    Co-authored-by: Lisa White <whitelisab@gmail.com>
    Co-authored-by: Gavin Matthews <gavindmatthews@gmail.com>

---
+++ apps/voucherify/src/contentful-constants.js
@@ -0,0 +1,9 @@
+export const ITEMS_PER_PAGE = 6;
+
+export const ALLOWED_RESOURCE_TYPES = {
+  CAMPAIGNS: 'campaigns',
+  EARNING_RULES: 'earning_rules',
+  PROMOTION_TIERS: 'promotion_tiers',
+};
+
+export const CUSTOM_URL_PATTERN = /^https:\/\/[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}(:[0-9]+)?(\/S*)?$/;
