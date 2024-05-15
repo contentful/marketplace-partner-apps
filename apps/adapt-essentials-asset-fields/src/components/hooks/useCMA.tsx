0b99fbb99073d44606367d0d5fd829053544037a
Darius Zivertas <2293236+zzzarius@users.noreply.github.com>
2024-05-15 18:06:42 +0300

    fix: Wrap cma with useMemo to prevent unnecessary renderings

--- apps/adapt-essentials-asset-fields/src/components/hooks/useCMA.tsx
+++ apps/adapt-essentials-asset-fields/src/components/hooks/useCMA.tsx
@@ -1,16 +1,22 @@
-import { createClient } from 'contentful-management'
+import { createClient } from 'contentful-management';
 import { useSDK } from '@contentful/react-apps-toolkit';
+import { useMemo } from 'react';
 
 export function useCMA() {
   const sdk = useSDK();
-  return createClient(
-    { apiAdapter: sdk.cmaAdapter },
-    {
-      type: 'plain',
-      defaults: {
-        environmentId: sdk.ids.environmentAlias ?? sdk.ids.environment,
-        spaceId: sdk.ids.space,
-      },
-    });
-
+  const cma = useMemo(
+    () =>
+      createClient(
+        { apiAdapter: sdk.cmaAdapter },
+        {
+          type: 'plain',
+          defaults: {
+            environmentId: sdk.ids.environmentAlias ?? sdk.ids.environment,
+            spaceId: sdk.ids.space,
+          },
+        },
+      ),
+    [sdk.cmaAdapter, sdk.ids.environment, sdk.ids.environmentAlias, sdk.ids.space],
+  );
+  return cma;
 }
