4652f918e7afd750c6eda092ebe8ccf68b2cdf74
Tyler Collins <tyler.collins@contentful.com>
2024-08-05 16:30:15 -0600

    chore: run codemod & fix build issues

---
+++ apps/imageHotspotCreator/vite-env.d.ts
@@ -0,0 +1,5 @@
+/// <reference types='vite/client' />
+declare module '@mui/icons-material/Cancel';
+declare module 'clone-deep';
+declare module '@mui/icons-material/Crop';
+declare module 'react-magic-dropzone';
