72b8de06bcf3525a8370366d8506431f48bd85c9
Tyler Collins <tyler.collins@contentful.com>
2024-08-06 12:27:26 -0600

    chore: deprecate vulnerable dependencies for bynder

---
+++ apps/bynder/vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  base: '', // relative paths
+  build: {
+    outDir: 'build',
+  },
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'happy-dom',
+  },
+});
