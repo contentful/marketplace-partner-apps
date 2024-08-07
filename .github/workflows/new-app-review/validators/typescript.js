04cb9018e274ab3c9c72f4182e7d689ceff86ec0
Lisa White <lisa.white@contentful.com>
2024-08-07 13:32:31 -0600

    add typescript check

--- .github/workflows/new-app-review/validators/typescript.js
+++ .github/workflows/new-app-review/validators/typescript.js
@@ -1,9 +1,22 @@
 module.exports = {
   validate: async ({ github, context, core }, newAppDir, files) => {
-    const usesTypescript = false;
+    const hasTsConfig = !!files.find((file) => file.status === 'added' && file.filename.startsWith(`${newAppDir}/tsconfig.json`));
+    const hasTsFiles = !!files.find(
+      (file) => file.status === 'added' && file.filename.startsWith(newAppDir) && (file.filename.endsWith('.ts') || file.filename.endsWith('.tsx'))
+    );
+    const hasPackageJson = !!files.find((file) => file.status === 'added' && file.filename.startsWith(`${newAppDir}/package.json`));
+    let dependsOnTypescript = false;
+    if (hasPackageJson) {
+      const packageJson = require(`../../${newAppDir}/package.json`);
+      dependsOnTypescript = packageJson.devDependencies && packageJson.devDependencies.typescript;
+    }
 
     return {
-      result: usesTypescript,
+      result: hasTsConfig && hasTsFiles,
+      message:
+        hasTsConfig && hasTsFiles && dependsOnTypescript
+          ? 'TypeScript check passed'
+          : 'TypeScript check failed: please include a tsconfig.json file, install typescript as a dev dependency, and TypeScript files in your app directory',
     };
   },
 };
