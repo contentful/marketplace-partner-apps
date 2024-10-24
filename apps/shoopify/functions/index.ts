ee42780d6b510a49896e5d0d1e36d36b8501aa2e
Mitch Goudy <mitchell.goudy@contentful.com>
2024-10-24 15:37:23 -0600

    fix: use shopify app in function

--- apps/shoopify/functions/index.ts
+++ apps/shoopify/functions/index.ts
@@ -1,2 +1,45 @@
-export function handler(event: any, context: any) {
+const removeHttpsAndTrailingSlash = (url) => {
+  const protocol = /^https?:\/\/(www\.)?/;
+  const trailingSlash = /\/$/;
+
+  return url.replace(protocol, '').replace(trailingSlash, '');
+};
+
+export async function handler(event: any, context: any) {
+  const query = `
+  query searchProducts($query: String!) {
+    search(query: $query, first: 10, types: PRODUCT) {
+        edges {
+        node {
+            ... on Product {
+            id
+            title
+            }
+        }
+        }
+    }
+    }
+  `;
+
+  const { apiEndpoint, storefrontAccessToken } = context.appInstallationParameters;
+  const url = `https://${removeHttpsAndTrailingSlash(apiEndpoint)}/api/2024-10/graphql`;
+
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      'x-shopify-storefront-access-token': storefrontAccessToken,
+    },
+    body: JSON.stringify({ query, variables: { query: '' } }),
+  });
+
+  let data = await response.json();
+
+  console.log(data);
+
+  return {
+    items: data,
+    pages: {},
+  };
 }
