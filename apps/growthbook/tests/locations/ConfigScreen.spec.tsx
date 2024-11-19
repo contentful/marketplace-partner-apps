78134faeb38e20f7d4987a572bcf426909ce80b2
James Irwin <tallnerd@gmail.com>
2024-11-19 12:44:29 +0100

    Correctly wrap in act.

--- apps/growthbook/tests/locations/ConfigScreen.spec.tsx
+++ apps/growthbook/tests/locations/ConfigScreen.spec.tsx
@@ -9,56 +9,78 @@ jest.mock('@contentful/react-apps-toolkit', () => ({
 }));
 
 describe('ConfigScreen component', () => {
-  it('renders the component', () => {
-    const { getByText } = render(<ConfigScreen />);
+  let getByText: (text: string) => HTMLElement = () => {
+    throw new Error('getByText is undefined');
+  };
+
+  let getByLabelText: (text: string) => HTMLElement = () => {
+    throw new Error('getByLabelText is undefined');
+  };
+
+  let getByDisplayValue: (text: string) => HTMLElement = () => {
+    throw new Error('getByDisplayValue is undefined');
+  };
+
+  it('renders the component', async () => {
+    await act(async () => {
+      ({ getByText } = render(<ConfigScreen />));
+    });
     expect(getByText('App Config')).toBeInTheDocument();
   });
 
-  it('displays default Growthbook Server URL', () => {
-    const { getByDisplayValue } = render(<ConfigScreen />);
+  it('displays default Growthbook Server URL', async () => {
+    await act(async () => {
+      ({ getByDisplayValue } = render(<ConfigScreen />));
+    });
     expect(getByDisplayValue('https://api.growthbook.io')).toBeInTheDocument();
   });
 
-  it('updates Growthbook Server URL on input change', () => {
-    const { getByLabelText } = render(<ConfigScreen />);
+  it('updates Growthbook Server URL on input change', async () => {
+    await act(async () => {
+      ({ getByLabelText } = render(<ConfigScreen />));
+    });
     const input = getByLabelText('Growthbook API Server URL (defaults to Growthbook Cloud api.growthbook.io)', { selector: 'input' }) as HTMLInputElement;
     fireEvent.change(input, { target: { value: 'https://new-url.com' } });
     expect(input.value).toBe('https://new-url.com');
   });
 
-  it('updates Growthbook API Key on input change, then obfuscates on blur, clears it on re-focus, an returns it to obfuscation if blurred again without modification', () => {
-    const { getByLabelText } = render(<ConfigScreen />);
+  it('updates Growthbook API Key on input change, then obfuscates on blur, clears it on re-focus, and returns it to obfuscation if blurred again without modification', async () => {
+    await act(async () => {
+      ({ getByLabelText } = render(<ConfigScreen />));
+    });
     const input = getByLabelText('Growthbook API Key') as HTMLInputElement;
     fireEvent.change(input, { target: { value: 'new-api-key' } });
-    expect((input as HTMLInputElement).value).toBe('new-api-key');
+    expect(input.value).toBe('new-api-key');
     fireEvent.blur(input);
-    expect((input as HTMLInputElement).value).toBe('***********');
+    expect(input.value).toBe('***********');
     fireEvent.focus(input);
-    expect((input as HTMLInputElement).value).toBe('');
+    expect(input.value).toBe('');
     fireEvent.blur(input);
-    expect((input as HTMLInputElement).value).toBe('***********');
+    expect(input.value).toBe('***********');
   });
 
-  it('updates Datasource Id on input change', () => {
-    const { getByLabelText } = render(<ConfigScreen />);
+  it('updates Datasource Id on input change', async () => {
+    await act(async () => {
+      ({ getByLabelText } = render(<ConfigScreen />));
+    });
     const input = getByLabelText('Datasource Id (The datasource that tracking data gets sent to)') as HTMLInputElement;
     fireEvent.change(input, { target: { value: 'new-datasource-id' } });
     expect(input.value).toBe('new-datasource-id');
   });
 
   it('calls onConfigure when sdk.app.onConfigure is triggered', async () => {
-    render(<ConfigScreen />);
     await act(async () => {
-      await mockSdk.app.onConfigure.mock.calls[0][0]();
+      render(<ConfigScreen />);
     });
+    await mockSdk.app.onConfigure.mock.calls[0][0]();
     expect(mockSdk.app.onConfigure).toHaveBeenCalled();
   });
 
   it('sets parameters correctly', async () => {
-    const { getByLabelText } = render(<ConfigScreen />);
     await act(async () => {
-      await mockSdk.app.onConfigure.mock.calls[0][0]();
+      ({ getByLabelText } = render(<ConfigScreen />));
     });
+    await mockSdk.app.onConfigure.mock.calls[0][0]();
     const serverInput = getByLabelText('Growthbook API Server URL (defaults to Growthbook Cloud api.growthbook.io)') as HTMLInputElement;
     const apiKeyInput = getByLabelText('Growthbook API Key') as HTMLInputElement;
     const datasourceIdInput = getByLabelText('Datasource Id (The datasource that tracking data gets sent to)') as HTMLInputElement;
