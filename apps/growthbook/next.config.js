b08a1959935c764bf10f7a13c8ee58f8ca508099
James Irwin <tallnerd@gmail.com>
2024-12-10 15:14:53 +0100

    fixed assetPrefix

--- apps/growthbook/next.config.js
+++ apps/growthbook/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   basePath: '',
-  assetPrefix: '',
+  assetPrefix: './',
   reactStrictMode: true,
   swcMinify: true,
   output: 'export',
