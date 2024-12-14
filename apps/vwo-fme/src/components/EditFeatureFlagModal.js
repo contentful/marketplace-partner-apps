61edf5987ff5bbac66fa69c62bae56b30c71457a
Dyavari Dasharath <dyavari.dasharath@vwo.com>
2024-12-14 08:46:04 +0530

    fix: new app vwo app: replace use-methods with useReducer

--- apps/vwo-fme/src/components/EditFeatureFlagModal.js
+++ apps/vwo-fme/src/components/EditFeatureFlagModal.js
@@ -1,99 +1,103 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useReducer } from 'react';
 import { Textarea, Button, Modal, Form, FormControl, Grid, TextInput, ModalControls } from '@contentful/f36-components';
 import { css } from 'emotion';
-import useMethods from 'use-methods';
 
 const styles = {
-   fieldItem: css({
-      marginBottom: '20px'
-   })
-}
+  fieldItem: css({
+    marginBottom: '20px',
+  }),
+};
 
-const getInitialData = featureFlag => ({
-   flagName: featureFlag?.name || '',
-   description: featureFlag?.description || ''
- });
+const actionTypes = {
+  SET_FLAG_NAME: 'SET_FLAG_NAME',
+  SET_DESCRIPTION: 'SET_DESCRIPTION',
+  RESET_STATE: 'RESET_STATE',
+};
 
- const methods = state => {
-   return {
-      setFlagName(flagName){
-         state.flagName = flagName;
-      },
-      setDescription(description){
-         state.description = description;
-      }
-   }
- }
+// Initial state function
+const getInitialState = (featureFlag) => ({
+  flagName: featureFlag?.name || '',
+  description: featureFlag?.description || '',
+});
+
+const setFlagName = (flagName) => ({ type: actionTypes.SET_FLAG_NAME, payload: flagName });
+const setDescription = (description) => ({ type: actionTypes.SET_DESCRIPTION, payload: description });
+const resetState = (featureFlag) => ({
+  type: actionTypes.RESET_STATE,
+  payload: getInitialState(featureFlag),
+});
+
+// Reducer function
+const reducer = (state, action) => {
+  switch (action.type) {
+    case actionTypes.SET_FLAG_NAME:
+      return { ...state, flagName: action.payload };
+    case actionTypes.SET_DESCRIPTION:
+      return { ...state, description: action.payload };
+    case actionTypes.RESET_STATE:
+      return action.payload;
+    default:
+      return state;
+  }
+};
 
 function EditFeatureFlagModal(props) {
-   const globalState = useMethods(methods, getInitialData(props.featureFlag));
-   const [state,actions] = globalState;
+  const [state, dispatch] = useReducer(reducer, getInitialState(props.featureFlag));
+  
+  const madeChanges = () => {
+    const { flagName, description } = state;
+    const { featureFlag } = props;
+    if (!flagName || !description) {
+      return false;
+    }
+    return flagName !== featureFlag.name || description !== featureFlag.description;
+  };
 
-   const updateFeatureFlag = (featureFlag) => {
-      if(!featureFlag){
-         return;
-      }
-      actions.setFlagName(featureFlag.name);
-      actions.setDescription(featureFlag.description);
-   }
-
-   const madeChanges = () => {
-      if(!state.flagName || !state.description){
-         return false;
-      }
-      return state.flagName !== props.featureFlag.name || state.description !== props.featureFlag.description;
-   }
-   
-   useEffect(() => {
-      updateFeatureFlag(props.featureFlag);
-   },[props.featureFlag])
+  useEffect(() => {
+    dispatch(resetState(props.featureFlag));
+  }, [props.featureFlag]);
 
   return (
     <React.Fragment>
-      <Modal onClose={() => props.onModalClose('')} isShown={props.isShown} size='medium'>
-      {() => (
-         <>
-            <Modal.Header
-               title="Edit Feature flag details"
-               onClose={() => props.onModalClose('')}
-            />
+      <Modal onClose={() => props.onModalClose('')} isShown={props.isShown} size="medium">
+        {() => (
+          <>
+            <Modal.Header title="Edit Feature flag details" onClose={() => props.onModalClose('')} />
             <Modal.Content>
-               <Form onSubmit={() => props.onModalClose(state)}>
-               <FormControl className={styles.fieldItem}>
-                     <FormControl.Label isRequired>Flag name</FormControl.Label>
-                     <TextInput
-                        value={state.flagName}
-                        maxLength='255'
-                        onChange={(e) => actions.setFlagName(e.target.value)}/>
-                     <Grid columns="auto 80px">
-                        <FormControl.HelpText>
-                           Name should be no longer than 255 characters
-                        </FormControl.HelpText>
-                        <FormControl.Counter />
-                     </Grid>
-                  </FormControl>
-                  <FormControl className={styles.fieldItem}>
-                     <FormControl.Label>Description</FormControl.Label>
-                     <Textarea
-                        value={state.description}
-                        maxLength='300'
-                        onChange={(e) => actions.setDescription(e.target.value)}/>
-                     <Grid columns="auto 80px">
-                        <FormControl.HelpText>
-                           Description should be no longer than 300 characters
-                        </FormControl.HelpText>
-                        <FormControl.Counter />
-                     </Grid>
-                  </FormControl>
-               </Form>
+              <Form onSubmit={() => props.onModalClose(state)}>
+                <FormControl className={styles.fieldItem}>
+                  <FormControl.Label isRequired>Flag name</FormControl.Label>
+                  <TextInput
+                    value={state.flagName}
+                    maxLength="255"
+                    onChange={(e) => dispatch(setFlagName(e.target.value))}
+                  />
+                  <Grid columns="auto 80px">
+                    <FormControl.HelpText>
+                      Name should be no longer than 255 characters
+                    </FormControl.HelpText>
+                    <FormControl.Counter />
+                  </Grid>
+                </FormControl>
+                <FormControl className={styles.fieldItem}>
+                  <FormControl.Label>Description</FormControl.Label>
+                  <Textarea
+                    value={state.description}
+                    maxLength="300"
+                    onChange={(e) => dispatch(setDescription(e.target.value))}
+                  />
+                  <Grid columns="auto 80px">
+                    <FormControl.HelpText>
+                      Description should be no longer than 300 characters
+                    </FormControl.HelpText>
+                    <FormControl.Counter />
+                  </Grid>
+                </FormControl>
+              </Form>
             </Modal.Content>
             <ModalControls>
-              <Button
-                size="small"
-                variant="transparent"
-                onClick={() => props.onModalClose('')}
-              >
-               Close
+              <Button size="small" variant="transparent" onClick={() => props.onModalClose('')}>
+                Close
               </Button>
               <Button
                 size="small"
@@ -102,14 +106,14 @@ function EditFeatureFlagModal(props) {
                 isDisabled={!madeChanges()}
                 onClick={() => props.onModalClose(state)}
               >
-               Save
+                Save
               </Button>
             </ModalControls>
-         </>
-         )}
+          </>
+        )}
       </Modal>
     </React.Fragment>
-  )
+  );
 }
 
 export default EditFeatureFlagModal;
