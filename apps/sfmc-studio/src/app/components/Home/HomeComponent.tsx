f03be763293bcaeb54281cb73a20b6db88905ab7
david-shibley-contentful <149433784+david-shibley-contentful@users.noreply.github.com>
2025-02-18 13:29:43 -0700

    fix: removing dead code [INTEG-2359] (#4385)

--- apps/sfmc-studio/src/app/components/Home/HomeComponent.tsx
+++ apps/sfmc-studio/src/app/components/Home/HomeComponent.tsx
@@ -2,22 +2,12 @@
 import React, { useMemo } from 'react';
 import { locations } from '@contentful/app-sdk';
 import ConfigScreen from '../Locations/ConfigScreen';
-import Field from '../Locations/Field';
-import EntryEditor from '../Locations/EntryEditor';
-import Dialog from '../Locations/Dialog';
-import Sidebar from '../Locations/Sidebar';
 import Page from '../Locations/Page';
-import Home from '../Locations/Home';
 import { useSDK } from '@contentful/react-apps-toolkit';
 
 const ComponentLocationSettings = {
   [locations.LOCATION_APP_CONFIG]: ConfigScreen,
-  [locations.LOCATION_ENTRY_FIELD]: Field,
-  [locations.LOCATION_ENTRY_EDITOR]: EntryEditor,
-  [locations.LOCATION_DIALOG]: Dialog,
-  [locations.LOCATION_ENTRY_SIDEBAR]: Sidebar,
   [locations.LOCATION_PAGE]: Page,
-  [locations.LOCATION_HOME]: Home,
 };
 
 const HomeComponent = () => {
