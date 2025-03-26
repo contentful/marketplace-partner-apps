707e735bc220746a6f5671d87245f82e9c0d71f1
Greg Huels <greg.huels@geteppo.com>
2025-03-26 13:54:03 -0500

    fix(eppo): enforce content-type validations in variants []

--- apps/eppo/src/hooks/useEntryEditorPermissions.tsx
+++ apps/eppo/src/hooks/useEntryEditorPermissions.tsx
@@ -2,10 +2,14 @@ import { EditorAppSDK } from '@contentful/app-sdk';
 import { useEditorPermissions } from '../components/modified/useEditorPermissions';
 import { useMemo } from 'react';
 
-export const useEntryEditorPermissions = (sdk: EditorAppSDK) => {
+export const useEntryEditorPermissions = (
+  sdk: EditorAppSDK,
+  allowedContentTypes: Array<string>,
+) => {
   const contentTypes = useMemo(() => {
     return sdk.space.getCachedContentTypes();
   }, [sdk.space]);
+
   return useEditorPermissions({
     access: sdk.access,
     entityType: 'Entry',
@@ -17,5 +21,8 @@ export const useEntryEditorPermissions = (sdk: EditorAppSDK) => {
       },
     },
     allContentTypes: contentTypes,
+    validations: {
+      contentTypes: allowedContentTypes,
+    },
   });
 };
