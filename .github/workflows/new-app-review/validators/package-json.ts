2081277a1326b4ee8add7446197a1de0141e7e4d
david shibley <david.shibley@contentful.com>
2025-03-26 11:57:10 -0600

    removing console logs, cleaning up script messaging

--- .github/workflows/new-app-review/validators/package-json.ts
+++ .github/workflows/new-app-review/validators/package-json.ts
@@ -3,18 +3,18 @@ import type { PullRequestFile, ValidatorOptions, ValidatorResult } from '../../t
 
 export const validate = async (_options: ValidatorOptions, newAppDir: string, files: PullRequestFile[]): Promise<ValidatorResult> => {
   const requiredScripts = ['start', 'build', 'test', 'lint'];
-  let hasScripts = false;
-  let isCorrectVersion = false;
-  let hasDeployScript = false;
-  let hasPassWithNoTests = false;
+  let hasScripts, isCorrectVersion, hasDeployScript, hasPassWithNoTests = false;
+  let missingScripts: string[] = [];
 
   if (await hasPackageJson(files, newAppDir)) {
     const packageJson = await import(packageJsonPath(newAppDir));
 
-    hasScripts = requiredScripts.every((script) => packageJson.scripts && packageJson.scripts[script]);
+    missingScripts = requiredScripts.filter((script) => !packageJson.scripts || !packageJson.scripts[script]);
+    hasScripts = missingScripts.length === 0;
     isCorrectVersion = packageJson.version.startsWith('0');
     hasDeployScript = packageJson.scripts && packageJson.scripts['deploy'];
     hasPassWithNoTests = packageJson.scripts && packageJson.scripts['test'] && packageJson.scripts['test'].includes('--passWithNoTests');
+
   }
 
   const result = hasScripts && isCorrectVersion && !hasDeployScript;
@@ -23,7 +23,7 @@ export const validate = async (_options: ValidatorOptions, newAppDir: string, fi
   if (!result) {
     let failureMessage = '';
     if (!hasScripts) {
-      failureMessage += `package.json is missing one or more of the following scripts: ${requiredScripts.join(', ')}. `;
+      failureMessage += `package.json is missing the following scripts: ${missingScripts.join(', ')}. `;
     }
     if (!isCorrectVersion) {
       failureMessage += 'package.json version should be less than 1.0. ';
