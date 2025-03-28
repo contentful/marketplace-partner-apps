cd348e85fe45faf1445d929801cc38d157505497
Chris Normansell <norman@gathercontent.com>
2025-03-28 13:58:08 +0000

    fix: Italics in content causes a validation error when importing

--- apps/bynder-content-workflow/src/appVersion/appVersion.ts
+++ apps/bynder-content-workflow/src/appVersion/appVersion.ts
@@ -1,4 +1,4 @@
 // This is the app version that will be updated manually using Semver scheme
 // App version will be sent to Content Workflow API inside some custom User-Agent header
-export const appVersion = "1.0.6";
+export const appVersion = "1.0.9";
 export const appName = "Content Workflow integration"
--- apps/bynder-content-workflow/vite.config.ts
+++ apps/bynder-content-workflow/vite.config.ts
@@ -1,20 +1,20 @@
-import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
-import path from 'path';
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import path from "path";
 
 export default defineConfig(() => ({
-  base: '', // relative paths
+  base: "", // relative paths
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src/'),
+      "@": path.resolve(__dirname, "./src/"),
     },
   },
   server: {
     port: 3000,
   },
   test: {
-    environment: 'happy-dom',
+    environment: "happy-dom",
   },
   build: {
     outDir: 'build',
