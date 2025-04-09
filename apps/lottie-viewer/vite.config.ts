252bdb09de4b3c76533a0e53903a414a1a9ae02c
david shibley <david.shibley@contentful.com>
2025-04-09 12:53:25 -0600

    feat: lottie viewer unit tests

--- apps/lottie-viewer/vite.config.ts
+++ apps/lottie-viewer/vite.config.ts
@@ -1,10 +1,14 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-
+import { defineConfig as defineVitest } from 'vitest/config';
 export default defineConfig(() => ({
-  base: '', // relative paths
-  server: {
+base: '', // relative paths
+server: {
     port: 3000,
-  },
-  plugins: [react()],
+},
+plugins: [react()],
+test: {
+    environment: 'jsdom',
+    globals: true
+}
 }));
