5ee92fc45ea48ef6469425f11134d4e49ff7669b
david-shibley-contentful <149433784+david-shibley-contentful@users.noreply.github.com>
2025-04-10 15:33:32 -0600

    fix: update typescript workflow [INTEG-2519] (#4832)

--- .github/workflows/new-app-review/validators/typescript.ts
+++ .github/workflows/new-app-review/validators/typescript.ts
@@ -3,8 +3,15 @@ import type { PullRequestFile, ValidatorOptions, ValidatorResult } from '../../t
 
 export const validate = async (_options: ValidatorOptions, newAppDir: string, files: PullRequestFile[]): Promise<ValidatorResult> => {
   const hasTsConfig = !!files.find((file) => file.status === 'added' && file.filename.includes(`tsconfig.json`));
+
+  console.log("files", files.map((file) => file.filename));
+  console.log("files", files.map((file) => file.filename.includes(`tsconfig.json`)));
   const hasTsFiles = !!files.find(
     (file) => file.status === 'added' && file.filename.startsWith(newAppDir) && (file.filename.endsWith('.ts') || file.filename.endsWith('.tsx'))
+  );
+
+  const hasJsFiles = !!files.find(
+    (file) => file.status === 'added' && file.filename.startsWith(newAppDir) && (file.filename.endsWith('.js'))
   );
 
   let dependsOnTypescript = false;
@@ -21,13 +28,16 @@ export const validate = async (_options: ValidatorOptions, newAppDir: string, fi
   console.log("dependsOnTypescript", dependsOnTypescript);
 
   const result = hasTsConfig && hasTsFiles;
-  const message =
-    result && dependsOnTypescript
-      ? 'TypeScript check passed'
-      : 'TypeScript check failed: please include a tsconfig.json file, install typescript as a dev dependency, and include TypeScript files in your app directory';
+  let message = result && dependsOnTypescript
+    ? 'TypeScript check passed'
+    : 'TypeScript check failed: please include a tsconfig.json file, install typescript as a dev dependency, and include TypeScript files in your app directory';
 
-      // currently only warning about typescript validation
-      // update this when we want to enforce typescript
+  if (hasJsFiles) {
+    message += ' Note: JavaScript files are present in the directory.';
+  }
+
+  // currently only warning about typescript validation
+  // update this when we want to enforce typescript
   return {
     result: true,
     warning: message,
