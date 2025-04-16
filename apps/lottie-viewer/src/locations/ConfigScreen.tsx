4e2d63a2092e7c45a0f4ae4d9553712783e4a8cd
Sarah Lessner <sarah.lessner@contentful.com>
2025-04-16 11:30:10 -0600

    config page loads and filters content types to json fields

--- apps/lottie-viewer/src/locations/ConfigScreen.tsx
+++ apps/lottie-viewer/src/locations/ConfigScreen.tsx
@@ -3,100 +3,200 @@ import { Heading, Box, Paragraph, List, Note, TextLink, Autocomplete, Flex, Chec
 import tokens from '@contentful/f36-tokens';
 import { useSDK } from '@contentful/react-apps-toolkit';
 import { ContentTypeProps } from 'contentful-management';
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import css from '@emotion/css';
-import { FieldSelection } from '../components/FieldSelection';
-import { useSurferCompatibility } from '../hooks/useSurferCompatibility';
-import { useFieldSelection } from '../hooks/useFieldSelection';
-import { SurferLogotype } from '../components/SurferLogotype';
+// import { getRichTextFields, setAppRichTextEditor, setDefaultRichTextEditor } from '../utils';
+import { CMAClient } from '@contentful/app-sdk';
+// import { RtfField } from '../types';
+import AsyncLock from 'async-lock';
+import { J } from 'vitest/dist/types-198fd1d9';
+const lock = new AsyncLock();
 
-const styles = {
-  splitter: css({
-    marginTop: tokens.spacingL,
-    marginBottom: tokens.spacingL,
-    border: 0,
-    height: '2px',
-    backgroundColor: tokens.gray300,
-  }),
-  body: css({
-    height: 'auto',
-    minHeight: '65vh',
-    margin: '0 auto',
-    marginTop: tokens.spacingXl,
-    padding: `${tokens.spacingXl} ${tokens.spacing2Xl}`,
-    maxWidth: '900px',
-    backgroundColor: tokens.colorWhite,
-    zIndex: 2,
-    boxShadow: '0px 0px 20px rgba(0, 0, 0, 0.1)',
-    borderRadius: '2px',
-  }),
-  background: css({
-    display: 'block',
-    position: 'absolute',
-    zIndex: -1,
-    top: 0,
-    width: '100%',
-    height: '300px',
-    backgroundColor: '#250956',
-  }),
-};
+const JsonFieldType = 'Object';
+const AppWidgetNamespace = 'app';
+const BuiltinWidgetNamesspace = 'builtin';
+const DefaultWidgetId = 'richTextEditor';
 
-const Splitter = () => {
-  return <hr className={styles.splitter} />;
-};
+export async function getJsonFields(cma: CMAClient, appDefinitionId: string): Promise<any[]> {
+  // Get all content types
+  const contentTypes = await cma.contentType.getMany({});
 
-const embedInSidebar = (selectedContentTypes: string[]) => {
-  return {
-    EditorInterface: selectedContentTypes.reduce((acc: AppState['EditorInterface'], id) => {
-      acc[id] = { sidebar: { position: 1 } };
-
-      return acc;
-    }, {}),
-  };
-};
-
-export const ConfigScreen = () => {
-  const [contentTypes, setContentTypes] = useState<ContentTypeProps[]>();
-
-  const compatibility = useSurferCompatibility(contentTypes);
-  const { selectMany, selectAllCompatible, selectedContentTypes, toggleContentType, toggleField, selectedContentFields } = useFieldSelection(compatibility);
-  const sdk = useSDK<ConfigAppSDK>();
-
-  useEffect(() => {
-    sdk.cma.contentType.getMany({ spaceId: sdk.ids.space }).then((contentTypes) => {
-      setContentTypes(contentTypes.items);
-    });
-  }, [sdk.app, sdk.cma.contentType, sdk.ids.space]);
-
-  useEffect(() => {
-    if (contentTypes && compatibility) {
-      sdk.app.getCurrentState().then((appState) => {
-        if (appState?.EditorInterface) {
-          selectMany(Object.keys(appState.EditorInterface), sdk.parameters.installation.selectedContentFields ?? compatibility.compatibleFields);
-        } else {
-          selectAllCompatible();
-        }
-
-        sdk.app.setReady();
+  const jsonFields: any[] = [];
+  for (const contentType of contentTypes.items) {
+    const editorInterface = await cma.editorInterface.get({ contentTypeId: contentType.sys.id });
+    for (const jsonField of contentType.fields.filter((f) => f.type === JsonFieldType)) {
+      const control = editorInterface.controls!.find((w) => w.fieldId === jsonField.id);
+      jsonFields.push({
+        contentTypeId: contentType.sys.id,
+        contentTypeName: contentType.name,
+        fieldId: jsonField.id,
+        fieldName: jsonField.name,
+        isEnabled: control!.widgetId === appDefinitionId,
       });
     }
-  }, [contentTypes, compatibility, sdk.app, selectMany, selectAllCompatible, sdk.parameters.installation.selectedContentFields]);
+  }
+
+  jsonFields.sort((a, b) => {
+    // Sort by contentTypeName first
+    const typeCompare = a.contentTypeName.localeCompare(b.contentTypeName);
+    if (typeCompare !== 0) {
+      return typeCompare;
+    }
+
+    // Then by fieldName
+    return a.fieldName.localeCompare(b.fieldName);
+  });
+
+  return jsonFields;
+}
+
+export async function setFieldAppearance(sdk: ConfigAppSDK, contentTypeId: string, fieldId: string) {
+  lock.acquire(contentTypeId, async function () {
+    const appWidgetId = sdk.ids.app;
+
+    const editorInterface = await sdk.cma.editorInterface.get({ contentTypeId: contentTypeId });
+    const control = editorInterface.controls!.find((w) => w.fieldId === fieldId)!;
+    control.widgetId = appWidgetId;
+    control.widgetNamespace = AppWidgetNamespace;
+
+    await sdk.cma.editorInterface.update(
+      {
+        spaceId: sdk.ids.space,
+        environmentId: sdk.ids.environment,
+        contentTypeId: contentTypeId,
+      },
+      editorInterface
+    );
+  });
+}
+
+export async function resetFieldAppearance(sdk: ConfigAppSDK, contentTypeId: string, fieldId: string) {
+  lock.acquire(contentTypeId, async function () {
+    const editorInterface = await sdk.cma.editorInterface.get({ contentTypeId: contentTypeId });
+    const control = editorInterface.controls!.find((w) => w.fieldId === fieldId)!;
+    control.widgetId = DefaultWidgetId;
+    control.widgetNamespace = BuiltinWidgetNamesspace;
+
+    await sdk.cma.editorInterface.update(
+      {
+        spaceId: sdk.ids.space,
+        environmentId: sdk.ids.environment,
+        contentTypeId: contentTypeId,
+      },
+      editorInterface
+    );
+  });
+}
+
+async function getContentModels(sdk: ConfigAppSDK) {
+  const types = await sdk.cma.contentType.getMany({});
+  return types.items.map((x) => {
+    return { label: x.name, value: x.sys.id };
+  });
+}
+const ConfigScreen = () => {
+  const [parameters, setParameters] = useState<any>({
+    useImageWrapper: false,
+    imageWrapperTypeId: '',
+  });
+  const [selectedWrapperModelId, setSelectedWrapperModelId] = useState(parameters.imageWrapperTypeId);
+  const [contentModels, setContentModels] = useState<{ label: string; value: string }[]>([]);
+  const [jsonFields, setJsonFields] = useState<any[]>([]);
+  const [jsonFieldsLoaded, setJsonFieldsLoaded] = useState<boolean>(false);
+  const [isLicensed, setIsLicensed] = useState(false);
+  const [isInstalled, setIsInstalled] = useState(false);
+
+  console.log({ contentModels, jsonFields });
+
+  const sdk = useSDK<ConfigAppSDK>();
+
+  const onConfigure = useCallback(async () => {
+    const currentState = await sdk.app.getCurrentState();
+    return {
+      parameters,
+      targetState: currentState,
+    };
+  }, [parameters, sdk]);
 
   useEffect(() => {
-    sdk.app.onConfigure(() => {
-      return {
-        targetState: embedInSidebar(selectedContentTypes),
-        parameters: {
-          selectedContentFields,
-        },
-      };
-    });
-  }, [sdk.app, selectedContentFields, selectedContentTypes]);
+    sdk.app.onConfigure(() => onConfigure());
+  }, [sdk, onConfigure]);
 
+  useEffect(() => {
+    if (parameters.imageWrapperTypeId && parameters.imageWrapperTypeId !== '') {
+      setSelectedWrapperModelId(parameters.imageWrapperTypeId);
+    }
+  }, [parameters]);
+
+  useEffect(() => {
+    (async () => {
+      const currentParameters: any | null = await sdk.app.getParameters();
+
+      if (currentParameters) {
+        setParameters(currentParameters);
+      }
+      sdk.app.setReady();
+
+      const models = await getContentModels(sdk);
+      setContentModels(models);
+
+      const fields = await getJsonFields(sdk.cma, sdk.ids.app);
+      setJsonFields(fields);
+      setJsonFieldsLoaded(true);
+    })();
+  }, [sdk]);
+
+  useEffect(() => {
+    let intervalId: any;
+    (async () => {
+      // Initial check
+      const installed = await checkInstallationStatus();
+      if (installed) {
+        return;
+      }
+      // Polling
+      intervalId = setInterval(async () => {
+        if (await checkInstallationStatus()) {
+          clearInterval(intervalId);
+        }
+      }, 2000);
+    })();
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [sdk.app]);
+  async function checkInstallationStatus(): Promise<boolean> {
+    const installed = await sdk.app.isInstalled();
+    setIsInstalled(installed);
+    return installed;
+  }
+
+  async function enableEditorForAllRichTextFields() {
+    for (const field of jsonFields.filter((f) => !f.isEnabled)) {
+      updateRichTextField(field.contentTypeId, field.fieldId, { isEnabled: true });
+      await setFieldAppearance(sdk, field.contentTypeId, field.fieldId);
+    }
+  }
+
+  async function toggleRichTextFieldEditor(field: any) {
+    const isEnabled = !field.isEnabled;
+    updateRichTextField(field.contentTypeId, field.fieldId, { isEnabled });
+
+    if (isEnabled) {
+      await setFieldAppearance(sdk, field.contentTypeId, field.fieldId);
+    } else {
+      await resetFieldAppearance(sdk, field.contentTypeId, field.fieldId);
+    }
+  }
+
+  function updateRichTextField(targetContentTypeId: string, targetFieldId: string, updatedProperties: Partial<any>) {
+    setJsonFields((prevFields) =>
+      prevFields.map((field) => (field.contentTypeId === targetContentTypeId && field.fieldId === targetFieldId ? { ...field, ...updatedProperties } : field))
+    );
+  }
   return (
     <>
-      <Box className={styles.background} />
-      <Box className={styles.body}>
+      <Box>
         <>
           <Heading as="h2"> About Surfer</Heading>
 
@@ -111,50 +211,27 @@ export const ConfigScreen = () => {
             </TextLink>
             .
           </Note>
-          <Splitter />
         </>
-        <>
-          <Heading as="h2">Setup</Heading>
-          <List as="ol">
-            <List.Item>Log into your Surfer account.</List.Item>
-            <List.Item>Select content types you want Surfer App to work with</List.Item>
-            <List.Item>Finish configuration by clicking the "Save" button</List.Item>
-            <List.Item>You now can start editing your content in Contentful with best-in-class SEO suggestions from Surfer!</List.Item>
-          </List>
-          <Splitter />
-        </>
+
         <Heading>Select content types and fields</Heading>
 
-        <Note variant="neutral">
-          Each content type you'll select must contain at least one <code>RichText</code> field.
-        </Note>
-
         <Autocomplete
-          items={[{ name: 'inputValue', id: 'inputValue' }]}
+          items={[{ name: 'inputName', id: 'inputId' }]}
           renderItem={(item, inputValue) => {
-            // const { before, match, after } = getStringMatch(item.name, inputValue);
-            // const isSelectable = props.isResourceSelectable ? props.isResourceSelectable(item) : true;
-
             return (
               <Flex alignItems="center" gap={tokens.spacingXs} testId={`resource-autocomplete--${item.name}`}>
                 <Checkbox value={item.id} id={item.id} isChecked isDisabled={false} onKeyDown={() => {}} />
-                {/* {isSelectable ? (
-                  <Text>
-                    <Text fontWeight="fontWeightNormal">{before}</Text>
-                    <Text fontWeight="fontWeightDemiBold">{match}</Text>
-                    <Text fontWeight="fontWeightNormal">{after}</Text>
-                  </Text>
-                ) : (
-                  <Text fontColor="gray400">{item.name}</Text>
-                )} */}
               </Flex>
             );
           }}
           onInputValueChange={(e) => console.log(e)}
           onSelectItem={(item) => console.log(item)}
-          selectedItem={{ name: 'inputValue' }}
+          selectedItem={{ name: 'inputName' }}
+          itemToString={(item) => item.name}
         />
       </Box>
     </>
   );
 };
+
+export default ConfigScreen;
