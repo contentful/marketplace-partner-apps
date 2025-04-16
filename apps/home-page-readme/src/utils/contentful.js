97ee6383c2729843dc99ff75d171e23fe64e62b3
Shanon Place <shanonplace@gmail.com>
2025-04-15 20:50:38 -0700

    Initial commit of vite rebuild

---
+++ apps/home-page-readme/src/utils/contentful.js
@@ -0,0 +1,102 @@
+//import sample markdown files from the examples folder
+export const README_CONTENT_TYPE_ID = 'readmeMd';
+
+// Creates a new ReadMe content type if needed
+export async function createReadmeType(cma) {
+  const { sys } = await cma.contentType.createWithId(
+    { contentTypeId: README_CONTENT_TYPE_ID },
+    {
+      name: README_CONTENT_TYPE_ID,
+      description: 'Used to populate the space home area with the rendered markdown',
+      displayField: 'title',
+      fields: [
+        {
+          id: 'title',
+          name: 'Title',
+          type: 'Symbol',
+          localized: false,
+          required: true,
+          validations: [],
+          disabled: false,
+          omitted: false,
+        },
+        {
+          id: 'readMe',
+          name: 'ReadMe',
+          type: 'Text',
+          localized: false,
+          required: false,
+          validations: [],
+          disabled: false,
+          omitted: false,
+        },
+      ],
+    }
+  );
+
+  if (!sys) return;
+
+  console.log(sys);
+
+  //now publish the type
+  await cma.contentType.publish(
+    {
+      contentTypeId: sys.id,
+    },
+    {
+      sys: {
+        id: sys.id,
+        version: sys.version,
+      },
+    }
+  );
+
+  console.log('published');
+
+  return sys.id;
+}
+
+export async function createReadmeEntry(cma) {
+  const entry = await cma.entry.create(
+    {
+      contentTypeId: README_CONTENT_TYPE_ID,
+    },
+    {
+      fields: {
+        title: {
+          'en-US': 'README',
+        },
+        readMe: {
+          'en-US': '## Add your own README.md content entrie and have fun!',
+        },
+      },
+    }
+  );
+
+  await entry.publish();
+}
+
+// Need to make sure the readme content type exists
+export async function checkForReadmeType(cma) {
+  console.log('Checking for ReadMe content type');
+  const returnValue = await cma.contentType
+    .get({
+      contentTypeId: README_CONTENT_TYPE_ID,
+    })
+    .catch((err) => {});
+
+  return returnValue ? true : false;
+}
+
+export async function checkForReadmeEntries(cma) {
+  // Find the ReadMe entries
+  const { items } = await cma.entry
+    .getMany({
+      query: { content_type: README_CONTENT_TYPE_ID },
+    })
+    .catch((err) => {
+      return [];
+    });
+
+  return items;
+}
