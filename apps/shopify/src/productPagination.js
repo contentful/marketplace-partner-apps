87d054bafec8fdb71a9bf8fc8890ce804f52c0ea
Mitch Goudy <mitchell.goudy@contentful.com>
2025-06-04 10:57:50 -0600

    refactor: replaced the Buy SDK with Storefront

--- apps/shopify/src/productPagination.js
+++ apps/shopify/src/productPagination.js
@@ -6,15 +6,96 @@ const makePagination = async (sdk) => {
   const pagination = new BasePagination({
     sdk,
     dataTransformer: productDataTransformer,
-    fetchProducts: async function (search, PER_PAGE) {
-      const query = { query: `variants:['sku:${search}'] OR title:${search}` };
+    fetchProducts: async function (search, perPage, after = null) {
+      const queryStr = search.length ? `title:*${search}* OR variants.sku:*${search}*` : '';
 
-      const products = await this.shopifyClient.product.fetchQuery({
-        first: PER_PAGE,
-        sortBy: 'TITLE',
-        reverse: true,
-        ...(search.length && query),
+      const query = `
+        query getProducts($first: Int!, $after: String, $query: String) {
+          products(first: $first, after: $after, query: $query, sortKey: TITLE, reverse: true) {
+            edges {
+              node {
+                id
+                title
+                description
+                handle
+                createdAt
+                updatedAt
+                vendor
+                productType
+                tags
+                images(first: 10) {
+                  edges {
+                    node {
+                      id
+                      url
+                      altText
+                    }
+                  }
+                }
+                variants(first: 250) {
+                  edges {
+                    node {
+                      id
+                      title
+                      sku
+                      price {
+                        amount
+                        currencyCode
+                      }
+                      compareAtPrice {
+                        amount
+                        currencyCode
+                      }
+                      availableForSale
+                      image {
+                        id
+                        url
+                        altText
+                      }
+                    }
+                  }
+                }
+              }
+              cursor
+            }
+            pageInfo {
+              hasNextPage
+              endCursor
+            }
+          }
+        }
+      `;
+
+      const variables = {
+        first: perPage,
+        after,
+        query: queryStr,
+      };
+
+      const response = await this.shopifyClient.request(query, { variables });
+
+      // Update cursor for next page
+      if (response.data.products.pageInfo.hasNextPage) {
+        this.lastCursor = response.data.products.pageInfo.endCursor;
+      }
+
+      // Transform GraphQL response to match expected format
+      const products = response.data.products.edges.map((edge) => {
+        const product = edge.node;
+        // Transform images and variants to match old Buy SDK format
+        const images = product.images.edges.map((imgEdge) => ({ src: imgEdge.node.url }));
+        const variants = product.variants.edges.map((varEdge) => ({
+          ...varEdge.node,
+          image: varEdge.node.image ? { src: varEdge.node.image.url } : null,
+        }));
+
+        return {
+          ...product,
+          images,
+          variants,
+        };
       });
+
       return products.map((res) => convertProductToBase64(res));
     },
   });
--- apps/shopify/src/productVariantPagination.js
+++ apps/shopify/src/productVariantPagination.js
@@ -12,6 +12,7 @@ class Pagination {
 
   hasNextProductPage = false;
   products = [];
+  lastCursor = null;
 
   variants = [];
 
@@ -40,9 +41,7 @@ class Pagination {
     // If there is a satisfactory size of variants to fill the next
     // page there is no need to fetch any more products and extract their variants
     // until the next click on the "Load more" button
-    const nothingLeftToFetch =
-      (!!this.products.length && !last(this.products).hasNextPage) ||
-      (!this.freshSearch && !this.products.length);
+    const nothingLeftToFetch = (!!this.products.length && !this.hasNextProductPage) || (!this.freshSearch && !this.products.length);
     const hasEnoughVariantsToConsume = this.variants.length >= PER_PAGE || nothingLeftToFetch;
     if (hasEnoughVariantsToConsume) {
       const variants = this.variants.splice(0, PER_PAGE);
@@ -70,14 +69,12 @@ class Pagination {
    */
   async _fetchMoreProducts(search) {
     const noProductsFetchedYet = this.products.length === 0;
-    const nextProducts = noProductsFetchedYet
-      ? await this._fetchProducts(search)
-      : await this._fetchNextPage(this.products);
-    this.hasNextProductPage =
-      nextProducts.length > 0 && nextProducts.every((product) => product.hasNextPage);
+    const nextProducts = noProductsFetchedYet ? await this._fetchProducts(search) : await this._fetchNextPage(search);
 
-    const nextVariants = productsToVariantsTransformer(nextProducts);
-    this.products = uniqBy([...this.products, ...nextProducts], 'id');
+    this.hasNextProductPage = nextProducts.hasNextPage;
+
+    const nextVariants = productsToVariantsTransformer(nextProducts.products || nextProducts);
+    this.products = uniqBy([...this.products, ...(nextProducts.products || nextProducts)], 'id');
     this.variants = sortBy(uniqBy([...this.variants, ...nextVariants], 'id'), ['title', 'sku']);
 
     this.freshSearch = false;
@@ -89,12 +86,95 @@ class Pagination {
    * a new search term.
    */
   async _fetchProducts(search) {
-    const query = { query: `variants:['sku:${search}'] OR title:${search}` };
-    const products = await this.shopifyClient.product.fetchQuery({
+    const queryStr = search.length ? `title:*${search}* OR variants.sku:*${search}*` : '';
+
+    const query = `
+      query getProductsWithVariants($first: Int!, $after: String, $query: String) {
+        products(first: $first, after: $after, query: $query, sortKey: TITLE, reverse: true) {
+          edges {
+            node {
+              id
+              title
+              description
+              handle
+              createdAt
+              updatedAt
+              vendor
+              productType
+              tags
+              images(first: 10) {
+                edges {
+                  node {
+                    id
+                    url
+                    altText
+                  }
+                }
+              }
+              variants(first: 250) {
+                edges {
+                  node {
+                    id
+                    title
+                    sku
+                    price {
+                      amount
+                      currencyCode
+                    }
+                    compareAtPrice {
+                      amount
+                      currencyCode
+                    }
+                    availableForSale
+                    image {
+                      id
+                      url
+                      altText
+                    }
+                  }
+                }
+              }
+            }
+            cursor
+          }
+          pageInfo {
+            hasNextPage
+            endCursor
+          }
+        }
+      }
+    `;
+
+    const variables = {
       first: PER_PAGE,
-      sortBy: 'TITLE',
-      reverse: true,
-      ...(search.length && query),
+      after: null,
+      query: queryStr,
+    };
+
+    const response = await this.shopifyClient.request(query, { variables });
+
+    // Update cursor and hasNextPage for next pagination
+    const pageInfo = response.data.products.pageInfo;
+    this.hasNextProductPage = pageInfo.hasNextPage;
+    if (pageInfo.hasNextPage) {
+      this.lastCursor = pageInfo.endCursor;
+    }
+
+    // Transform GraphQL response to match expected format
+    const products = response.data.products.edges.map((edge) => {
+      const product = edge.node;
+      // Transform images and variants to match old Buy SDK format
+      const images = product.images.edges.map((imgEdge) => ({ src: imgEdge.node.url }));
+      const variants = product.variants.edges.map((varEdge) => ({
+        ...varEdge.node,
+        image: varEdge.node.image ? { src: varEdge.node.image.url } : null,
+      }));
+
+      return {
+        ...product,
+        images,
+        variants,
+      };
     });
 
     return products.map((product) => convertProductToBase64(product));
@@ -102,18 +182,114 @@ class Pagination {
 
   /**
    * This method is used when the user has already fetched a batch of products
-   * and now want to render the next page.
+   * and now want to render the next page using cursor-based pagination.
    */
-  async _fetchNextPage(products) {
-    const nextProductVariants = (await this.shopifyClient.fetchNextPage(products)).model;
-    return nextProductVariants.map((nextProductVariant) =>
-      convertProductToBase64(nextProductVariant)
-    );
+  async _fetchNextPage(search) {
+    if (!this.lastCursor) {
+      return { products: [], hasNextPage: false };
+    }
+
+    const queryStr = search.length ? `title:*${search}* OR variants.sku:*${search}*` : '';
+
+    const query = `
+      query getProductsWithVariants($first: Int!, $after: String, $query: String) {
+        products(first: $first, after: $after, query: $query, sortKey: TITLE, reverse: true) {
+          edges {
+            node {
+              id
+              title
+              description
+              handle
+              createdAt
+              updatedAt
+              vendor
+              productType
+              tags
+              images(first: 10) {
+                edges {
+                  node {
+                    id
+                    url
+                    altText
+                  }
+                }
+              }
+              variants(first: 250) {
+                edges {
+                  node {
+                    id
+                    title
+                    sku
+                    price {
+                      amount
+                      currencyCode
+                    }
+                    compareAtPrice {
+                      amount
+                      currencyCode
+                    }
+                    availableForSale
+                    image {
+                      id
+                      url
+                      altText
+                    }
+                  }
+                }
+              }
+            }
+            cursor
+          }
+          pageInfo {
+            hasNextPage
+            endCursor
+          }
+        }
+      }
+    `;
+
+    const variables = {
+      first: PER_PAGE,
+      after: this.lastCursor,
+      query: queryStr,
+    };
+
+    const response = await this.shopifyClient.request(query, { variables });
+
+    // Update cursor and hasNextPage for next pagination
+    const pageInfo = response.data.products.pageInfo;
+    this.hasNextProductPage = pageInfo.hasNextPage;
+    if (pageInfo.hasNextPage) {
+      this.lastCursor = pageInfo.endCursor;
+    }
+
+    // Transform GraphQL response to match expected format
+    const products = response.data.products.edges.map((edge) => {
+      const product = edge.node;
+      // Transform images and variants to match old Buy SDK format
+      const images = product.images.edges.map((imgEdge) => ({ src: imgEdge.node.url }));
+      const variants = product.variants.edges.map((varEdge) => ({
+        ...varEdge.node,
+        image: varEdge.node.image ? { src: varEdge.node.image.url } : null,
+      }));
+
+      return {
+        ...product,
+        images,
+        variants,
+      };
+    });
+
+    return {
+      products: products.map((product) => convertProductToBase64(product)),
+      hasNextPage: pageInfo.hasNextPage,
+    };
   }
 
   _resetPagination() {
     this.products = [];
     this.variants = [];
+    this.lastCursor = null;
     this.freshSearch = true;
   }
 }
