13e759657541b1a2eb1b20029535968863bd09ea
Phillip Austerfield <phillip.austerfield@idmedia.com>
2025-06-30 10:21:36 +0100

    generate an index.html file in the build artifacts

--- apps/translationstudio/next.config.js
+++ apps/translationstudio/next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  basePath: '',
+  assetPrefix: './',
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig;
