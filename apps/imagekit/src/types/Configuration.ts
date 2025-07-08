24825849056cfb24bd45dd9f5700aba014a2b0cd
Harshit Budhraja <harshitbudhraja1301@gmail.com>
2025-07-08 18:31:58 +0530

    chore: added imagekit app

---
+++ apps/imagekit/src/types/Configuration.ts
@@ -0,0 +1,11 @@
+export interface Parameters {
+  installationUuid: string;
+  folderPath: string;
+  collectionId: string;
+  fileType: string;
+  searchQuery: string;
+  allowMultipleSelections: boolean;
+  maxFileSelections: string;
+  defaultTransformation: string;
+  mediaQuality: string;
+}
