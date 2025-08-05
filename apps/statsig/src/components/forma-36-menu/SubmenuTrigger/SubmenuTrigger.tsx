9c77ec693f68944f774f40fe954cf4ed770f4f97
Sarah Lessner <sarah.lessner@contentful.com>
2025-08-05 13:08:56 -0600

    resolve build issue

--- apps/statsig/src/components/forma-36-menu/SubmenuTrigger/SubmenuTrigger.tsx
+++ apps/statsig/src/components/forma-36-menu/SubmenuTrigger/SubmenuTrigger.tsx
@@ -1,19 +1,18 @@
 import React, { forwardRef } from 'react';
 
-import { CommonProps } from '@contentful/f36-core';
+import { ExpandProps } from '@contentful/f36-core';
 import * as emotion from '@emotion/css';
 import { ChevronRightIcon } from '@contentful/f36-icons';
+import { MenuItem, MenuItemProps } from '../MenuItem/MenuItem';
+import { MenuTrigger } from '../MenuTrigger/MenuTrigger';
+import { useSubmenuContext } from '../SubmenuContext';
 import { getSubmenuTriggerStyles } from '../SubmenuTrigger/SubmenuTrigger.styles';
 
 const { cx } = emotion;
 
-import { MenuItem, MenuItemProps } from '../MenuItem/MenuItem';
-import { MenuTrigger } from '../MenuTrigger/MenuTrigger';
-import { useSubmenuContext } from '../SubmenuContext';
-
 export type SubmenuTriggerProps = Omit<MenuItemProps<'button'>, 'isInitiallyFocused' | 'as'>;
 
-const _SubmenuTrigger = (props: CommonProps<SubmenuTriggerProps>, ref: React.Ref<HTMLButtonElement>) => {
+const _SubmenuTrigger = (props: ExpandProps<SubmenuTriggerProps>, ref: React.Ref<HTMLButtonElement>) => {
   const { className, children } = props;
   const { getSubmenuTriggerProps, isOpen } = useSubmenuContext()!;
 
