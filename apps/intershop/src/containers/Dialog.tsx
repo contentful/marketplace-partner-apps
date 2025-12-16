e603ef7a944f8d49f3145ec14642e8bab530fe9e
bert-vandepoele <54333611+bert-vandepoele@users.noreply.github.com>
2025-12-16 23:06:46 +0100

    fix: Flat Category Selection Implementation & Update new api specs [] (#7520)

--- apps/intershop/src/containers/Dialog.tsx
+++ apps/intershop/src/containers/Dialog.tsx
@@ -2,20 +2,10 @@ import React, { useCallback, useEffect, useState } from "react";
 import DialogComponent from "../components/Dialog";
 import { Product } from "../types/Product";
 import { Category } from "../types/Category";
-import { CategoryJson } from "../types/CategoryJson";
 import { MappedProductJson } from "../types/MappedProductJson";
 import jsonMapper from "../utils/JsonMapper";
 import { Blueprint } from "../types/Blueprint";
 import { replaceChannelAndApplication } from "../utils/replace";
-
-interface SearchableCategoryJson extends CategoryJson {
-  [key: string]:
-    | string
-    | boolean
-    | Array<string>
-    | Array<CategoryJson>
-    | undefined;
-}
 
 interface CategoryMetaData {
   categoryPath: Array<string>;
@@ -50,16 +40,16 @@ interface Props {
   channel: string;
   context: string;
   imageBase: string;
+  imageType: string;
   categoryBlueprint: Blueprint;
   productBlueprint: Blueprint;
   productCategoryBlueprint: Blueprint;
   selectedProductSkus: Array<string>;
-  selectedCategories: Array<CategoryJson>;
+  selectedCategories: Array<string>;
   onSaveSubmit: () => void;
   onCategorySelect: (
     id: string,
     categoryPath: Array<string>,
-    subCategoryTree: Array<any>
   ) => void;
   onProductSelect: (
     sku: string,
@@ -72,6 +62,7 @@ interface Props {
 const Dialog = ({
   apiBase,
   imageBase,
+  imageType,
   categoryBlueprint,
   productBlueprint,
   productCategoryBlueprint,
@@ -110,24 +101,6 @@ const Dialog = ({
     {}
   );
 
-  const getSelectedCategoriesKey = useCallback(
-    <T extends boolean | string | Array<string> | Array<CategoryJson>>(
-      key: string,
-      categories: Array<SearchableCategoryJson>
-    ): T[] =>
-      categories.reduce<Array<T>>((acc, category) => {
-        acc.push(category[key] as T);
-        acc.push(
-          ...getSelectedCategoriesKey<T>(
-            key,
-            category.subcategories as Array<SearchableCategoryJson>
-          )
-        );
-        return acc;
-      }, []),
-    []
-  );
-
   const mapCategories = useCallback(
     (categoriesToMap: Array<any>): Array<Category> =>
       categoriesToMap.map(({ totalProducts, subCategories, ...category }) => {
@@ -160,7 +133,7 @@ const Dialog = ({
 
         return {
           ...product,
-          image: `${imageBase}${image}`,
+          image,
           price: `$${price}`,
         };
       }),
@@ -184,7 +157,7 @@ const Dialog = ({
         .then(({ elements }: any) =>
           elements.map((element: any) => jsonMapper(categoryBlueprint, element))
         ),
-    [apiBase, application, categoryBlueprint, channel]
+    [apiBase, imageType, application, categoryBlueprint, channel]
   );
 
   const fetchProducts = useCallback(
@@ -208,8 +181,8 @@ const Dialog = ({
           }/products?${amount ? `amount=${amount}` : ""}&${
             offset ? `offset=${offset}` : ""
           }&${
-            skus ? `SKU=${skus}` : ""
-          }&searchTerm=${searchTerm}&attrs=sku,manufacturer,image,defaultCategory,listPrice`,
+            skus ? `sku=${skus}` : ""
+          }&searchTerm=${searchTerm}&attrs=sku,manufacturer,image@${imageType},defaultCategory,listPrice`,
           { channel, application }
         )
       )
@@ -235,7 +208,7 @@ const Dialog = ({
           }));
           setProductsLoading(false);
         }),
-    [apiBase, application, channel, productBlueprint]
+    [apiBase, imageType, application, channel, productBlueprint]
   );
 
   const loadCategories = useCallback(() => {
@@ -367,7 +340,6 @@ const Dialog = ({
       onCategorySelect(
         id,
         categoryPath,
-        subCategories.map((subCategoryId) => makeSubCategoryTree(subCategoryId))
       );
     },
     [categoriesMetaDeta, onCategorySelect]
@@ -414,45 +386,18 @@ const Dialog = ({
   }, [updateSelectedProducts]);
 
   useEffect(() => {
-    const mapCategoryExcludedProducts = (categories: Array<CategoryJson>) =>
-      categories.reduce<CategoryExcludedProducts>(
-        (acc, { category_id: id, excluded_products: excludedProducts }) => {
-          if (excludedProducts) {
-            acc[id] = excludedProducts;
-          }
-          return acc;
-        },
-        {}
-      );
-
-    setSelectedCategoryIds(
-      getSelectedCategoriesKey<string>(
-        "category_id",
-        selectedCategories as Array<SearchableCategoryJson>
-      )
-    );
-    const excludedProducts = getSelectedCategoriesKey<Array<string>>(
-      "excluded_products",
-      selectedCategories as Array<SearchableCategoryJson>
-    ).reduce(
-      (acc, excludedProducts) => [...acc, ...(excludedProducts ?? [])],
-      []
-    );
-    setExcludedProductSkus(excludedProducts);
-    setExcludedCategoryProductSkus(
-      mapCategoryExcludedProducts(selectedCategories)
-    );
-  }, [getSelectedCategoriesKey, selectedCategories]);
+    // selectedCategories is now a flat array of category IDs
+    setSelectedCategoryIds(selectedCategories);
+    // No excluded products tracking needed with flat array
+    setExcludedProductSkus([]);
+    setExcludedCategoryProductSkus({});
+  }, [selectedCategories]);
 
   useEffect(() => {
     if (initialLoad && apiBase && context) {
       if (context === "category") {
-        setSelectedCategoryIds(
-          getSelectedCategoriesKey<string>(
-            "category_id",
-            selectedCategories as Array<SearchableCategoryJson>
-          )
-        );
+        // selectedCategories is already a flat array of IDs
+        setSelectedCategoryIds(selectedCategories);
         setSelectedCategoryIdsInitialized(true);
       } else {
         initializeSelectedProducts();
@@ -462,8 +407,8 @@ const Dialog = ({
     }
   }, [
     apiBase,
+    imageType,
     context,
-    getSelectedCategoriesKey,
     initialLoad,
     initializeSelectedProducts,
     loadCategories,
