ed0f8bc66303a2848ae0491c066143e24e6f9788
Leonel Oliveira <loliveira@Leonel-Mac.local>
2026-02-10 09:22:44 -0500

    fix build

--- apps/flexfields/vite.config.mjs
+++ apps/flexfields/vite.config.mjs
@@ -1,5 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   base: '', // relative paths
@@ -7,6 +11,14 @@ export default defineConfig({
     outDir: 'build',
   },
   plugins: [react()],
+  resolve: {
+    alias: {
+      'is-hotkey': path.resolve(__dirname, 'node_modules/is-hotkey'),
+    },
+  },
+  optimizeDeps: {
+    include: ['is-hotkey'],
+  },
   test: {
     globals: true,
     browser: {
