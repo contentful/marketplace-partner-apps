ac70bba3e079c672b898f5e03ce3e04303a95986
Daniel Cohen <daniel@codeinvain.com>
2026-06-18 22:05:37 +0300

    unit test fix

--- apps/cloudinary2/src/utils.spec.ts
+++ apps/cloudinary2/src/utils.spec.ts
@@ -15,8 +15,8 @@ const installationParams: AppInstallationParameters = {
 };
 
 const baseAsset: CloudinaryAsset = {
-  url: 'http://static.scott-sports.com/image/upload/f_auto/q_auto/v1779450624/SPARK_RC_SCOTT_BIKE_TECH_0125.jpg',
-  secure_url: 'https://static.scott-sports.com/image/upload/f_auto/q_auto/v1779450624/SPARK_RC_SCOTT_BIKE_TECH_0125.jpg',
+  url: 'http://images.my-domain.com/image/upload/f_auto/q_auto/v1779450624/SPARK_RC_SCOTT_BIKE_TECH_0125.jpg',
+  secure_url: 'https://images.my-domain.com/image/upload/f_auto/q_auto/v1779450624/SPARK_RC_SCOTT_BIKE_TECH_0125.jpg',
   tags: [],
   type: 'upload',
   bytes: 1,
@@ -33,7 +33,7 @@ const baseAsset: CloudinaryAsset = {
 
 describe('getDeliveryHostname', () => {
   it('returns custom delivery hostnames', () => {
-    expect(getDeliveryHostname('https://static.scott-sports.com/image/upload/test')).toBe('static.scott-sports.com');
+    expect(getDeliveryHostname('https://images.my-domain.com/image/upload/test')).toBe('images.my-domain.com');
   });
 
   it('returns undefined for the default Cloudinary hostname', () => {
@@ -66,7 +66,7 @@ describe('buildDeliveryUrl', () => {
   it('builds edited URLs on the configured CNAME', () => {
     const url = buildDeliveryUrl(installationParams, baseAsset, 'c_crop,h_2250,w_2250,x_1750,y_0/c_scale,h_2250,w_2250/f_auto/q_auto');
 
-    expect(url).toContain('static.scott-sports.com');
+    expect(url).toContain('images.my-domain.com');
     expect(url).toContain('c_crop,h_2250,w_2250,x_1750,y_0');
     expect(url).not.toContain('res.cloudinary.com');
   });
@@ -78,8 +78,8 @@ describe('createEditedAsset', () => {
 
     expect(edited.original_secure_url).toBe(baseAsset.secure_url);
     expect(edited.original_url).toBe(baseAsset.url);
-    expect(edited.secure_url).toContain('static.scott-sports.com');
-    expect(edited.url).toContain('static.scott-sports.com');
+    expect(edited.secure_url).toContain('images.my-domain.com');
+    expect(edited.url).toContain('images.my-domain.com');
     expect(edited.raw_transformation).toBe('c_crop,h_2250,w_2250,x_1750,y_0/c_scale,h_2250,w_2250/f_auto/q_auto');
   });
 });
