d0e12e546374c394df7532a8847e9ab4fbaa488a
Tyler <tyler.washington@contentful.com>
2026-07-06 14:11:45 -0600

    fix(security): use contextual error message in translationstudio fetchAllEntries [ACT-3450]

--- apps/translationstudio/components/Translations.tsx
+++ apps/translationstudio/components/Translations.tsx
@@ -234,7 +234,7 @@ async function fetchAllEntries(sdk: PageAppSDK, space: string, id: string, displ
         return data;
     }
     catch (err) {
-        console.error('An error occurred. Please try again.');
+        console.error('Failed to fetch entries.');
     }
 
     return null;
