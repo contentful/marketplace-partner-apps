61b8ae0c8f936028cccbe419894e32d05f86c1ba
Hans-Robrecht <robreha@cronos.be>
2026-09-03 06:45:42 +0200

    refactor(intershop): Format ProductCard

--- apps/intershop/src/components/ProductCard.tsx
+++ apps/intershop/src/components/ProductCard.tsx
@@ -1,6 +1,6 @@
-import { Card, Flex, Text, Tooltip } from "@contentful/f36-components";
-import React, { CSSProperties } from "react";
-import CloseButton from "./CloseButton";
+import { Card, Flex, Text, Tooltip } from '@contentful/f36-components';
+import React, { CSSProperties } from 'react';
+import CloseButton from './CloseButton';
 
 interface ImageType {
   alt: string;
@@ -21,64 +21,32 @@ export interface Props {
   onClose?: () => void;
 }
 
-const ProductCard = ({
-  selected,
-  aria = "Close product",
-  identifier,
-  price,
-  subtitle,
-  title,
-  warning,
-  style,
-  image,
-  onClick,
-  onClose,
-}: Props) => {
+const ProductCard = ({ selected, aria = 'Close product', identifier, price, subtitle, title, warning, style, image, onClick, onClose }: Props) => {
   const card = (
     <Card
       isSelected={selected}
       onClick={onClick}
       style={{
-        position: "relative",
-        ...(warning ? { cursor: "not-allowed" } : {}),
+        position: 'relative',
+        ...(warning ? { cursor: 'not-allowed' } : {}),
         ...style,
-      }}
-    >
-      {onClose && (
-        <CloseButton
-          aria={aria}
-          onClick={onClose}
-          style={{ position: "absolute", top: "0.5em", right: "0.5em" }}
-        />
-      )}
-      <Flex
-        flexDirection="column"
-        justifyContent="space-between"
-        alignItems="center"
-        fullHeight
-      >
-        <Flex
-          flexDirection="column"
-          style={{ marginRight: "auto", maxWidth: "100%", height: "5.25em" }}
-        >
-          <Text
-            fontSize="fontSizeXl"
-            fontWeight="fontWeightDemiBold"
-            isTruncated
-          >
+      }}>
+      {onClose && <CloseButton aria={aria} onClick={onClose} style={{ position: 'absolute', top: '0.5em', right: '0.5em' }} />}
+      <Flex flexDirection="column" justifyContent="space-between" alignItems="center" fullHeight>
+        <Flex flexDirection="column" style={{ marginRight: 'auto', maxWidth: '100%', height: '5.25em' }}>
+          <Text fontSize="fontSizeXl" fontWeight="fontWeightDemiBold" isTruncated>
             {title}
           </Text>
           <Text
             fontSize="fontSizeM"
             lineHeight="lineHeightCondensed"
             style={{
-              maxHeight: "2.5em",
+              maxHeight: '2.5em',
               WebkitLineClamp: 2,
-              WebkitBoxOrient: "vertical",
-              display: "-webkit-box",
-              overflow: "hidden",
-            }}
-          >
+              WebkitBoxOrient: 'vertical',
+              display: '-webkit-box',
+              overflow: 'hidden',
+            }}>
             {subtitle}
           </Text>
           {identifier && (
@@ -92,18 +60,12 @@ const ProductCard = ({
           src={image.src}
           alt={image.alt}
           style={{
-            maxWidth: "100%",
-            height: "50%",
-            aspectRatio: "auto 50/50",
+            maxWidth: '100%',
+            height: '50%',
+            aspectRatio: 'auto 50/50',
           }}
         />
-        <Text
-          fontColor="blue700"
-          fontSize="fontSizeXl"
-          fontStack="fontStackMonospace"
-          fontWeight="fontWeightDemiBold"
-          style={{ marginLeft: "auto" }}
-        >
+        <Text fontColor="blue700" fontSize="fontSizeXl" fontStack="fontStackMonospace" fontWeight="fontWeightDemiBold" style={{ marginLeft: 'auto' }}>
           {price}
         </Text>
       </Flex>
